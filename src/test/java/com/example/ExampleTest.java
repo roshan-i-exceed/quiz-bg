e0174b92f6cb38a5830e949114ced99b06888e29
roshan.shetty <roshan.shetty@i-exceed.com>
2026-08-28 11:27:53 +0530

    first commit

---
+++ src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,49 @@
+package com.example;
+ 
+import org.junit.jupiter.api.Test;
+ 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+ 
+public class ExampleTest {
+ 
+    @Test
+    void quizAppFlow() {
+ 
+        Playwright playwright = Playwright.create();
+ 
+        Browser browser = playwright.chromium().launch(
+            new BrowserType.LaunchOptions()
+                .setHeadless(false)
+                .setDevtools(true)
+        );
+ 
+        BrowserContext context = browser.newContext();
+        Page page = context.newPage();
+ 
+        // Open application
+        page.navigate("http://localhost:8018/QuizFr/");
+ 
+        System.out.println("Quiz application opened successfully");
+        page.locator("html").click();
+    page.locator("#QuizFr__QuizzesList__el_btn_1_0").click();
+    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Ok")).click();
+    page.locator("#QuizFr__Questions__el_btn_4_1").click();
+    page.locator("#QuizFr__Questions__ct_lst_6_row_1 #QuizFr__Questions__sc_col_15_li").click();
+    page.locator("#QuizFr__Questions__el_btn_4_1").click();
+    page.getByText("What is the highest-grossing").click();
+    page.locator("#QuizFr__Questions__el_btn_4_2").click();
+    page.locator("#QuizFr__Questions__ct_lst_6_row_1 #QuizFr__Questions__sc_col_15_li").click();
+    page.locator("#QuizFr__Questions__el_btn_4_2").click();
+    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit")).click();
+    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Ok")).click();
+    page.locator("#td_QuizFr__Questions__el_btn_4_1").click();
+ 
+        // Keep browser open
+        page.pause();
+ 
+        // DO NOT close browser while inspecting
+        // browser.close();
+        // playwright.close();
+    }
+}
